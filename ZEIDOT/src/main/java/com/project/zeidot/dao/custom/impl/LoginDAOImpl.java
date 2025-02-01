d5c50d70966d43d67274837019a91004f271cdfb
Prabhashperera <prabashmperera01@gmail.com>
2025-02-01 23:42:28 +0530

    LOGIN COMPLETED 100% with LA

--- ZEIDOT/src/main/java/com/project/zeidot/dao/custom/impl/LoginDAOImpl.java
+++ ZEIDOT/src/main/java/com/project/zeidot/dao/custom/impl/LoginDAOImpl.java
@@ -4,6 +4,7 @@ import com.project.zeidot.dao.custom.LoginDAO;
 import com.project.zeidot.db.DBConnection;
 import com.project.zeidot.dto.UserDTO;
 import com.project.zeidot.entity.User;
+import com.project.zeidot.util.CrudUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -13,11 +14,8 @@ import java.sql.SQLException;
 public class LoginDAOImpl implements LoginDAO {
     @Override
     public boolean login(User entity) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement statement = connection.prepareStatement("select * from user where username = ? and password = ?");
-        statement.setString(1, entity.getUsername());
-        statement.setString(2, entity.getPassword());
-        ResultSet resultSet = statement.executeQuery(); //Get all the users into a resultSet
+        ResultSet resultSet = CrudUtil.execute("select * from user where username = ? and password = ?", entity.getUsername(), entity.getPassword() );
+        //Get all the users into a resultSet
         while (resultSet.next()) {
             String username = resultSet.getString("userName");
             String password = resultSet.getString("password");
@@ -30,11 +28,6 @@ public class LoginDAOImpl implements LoginDAO {
 
     @Override
     public boolean register(User entity) throws SQLException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement statement = connection.prepareStatement("insert into user values(?,?)");
-        statement.setString(1, entity.getUsername());
-        statement.setString(2, entity.getPassword());
-        int rows = statement.executeUpdate();
-        return rows > 0;
+       return CrudUtil.execute("insert into user values(?,?)" , entity.getUsername(), entity.getPassword());
     }
 }
