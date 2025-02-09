f095c1720908b2d7dac1db2d878837e8b43be746
Prabhashperera <prabashmperera01@gmail.com>
2025-02-09 21:03:51 +0530

    DonationSelect (DAO,BO) COMPLETED 100%

--- ZEIDOT/src/main/java/com/project/zeidot/bo/custom/PopupsBOs/DonationSelectBOImpl.java
+++ ZEIDOT/src/main/java/com/project/zeidot/bo/custom/PopupsBOs/DonationSelectBOImpl.java
@@ -3,17 +3,29 @@ package com.project.zeidot.bo.custom.PopupsBOs;
 import com.project.zeidot.dao.custom.DAOFactory;
 import com.project.zeidot.dao.custom.PopupsDAOS.PopupDAO;
 import com.project.zeidot.dto.DonationDto;
+import com.project.zeidot.entity.Donation;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class DonationSelectBOImpl implements PopupBO<DonationDto>{
 
-    private final PopupDAO<DonationDto> donationDtoPopupDAO =
-            (PopupDAO<DonationDto>) DAOFactory.getInstance().getDAOType(DAOFactory.DAOType.DONATION_SELECT);
+    private final PopupDAO<Donation> donationDtoPopupDAO =
+            (PopupDAO<Donation>) DAOFactory.getInstance().getDAOType(DAOFactory.DAOType.DONATION_SELECT);
 
     @Override
     public ArrayList<DonationDto> getDetails() throws SQLException {
-        return donationDtoPopupDAO.getDetails();
+        ArrayList<Donation> donations = donationDtoPopupDAO.getDetails();
+        ArrayList<DonationDto> donationDtos = new ArrayList<>();
+
+        for (Donation donation : donations) {
+            DonationDto dto = new DonationDto();
+            dto.setFBId(donation.getFBId());
+            dto.setDonationName(donation.getDonationName());
+            dto.setFoodBankID(donation.getFoodBankID());
+            dto.setDonationID(donation.getDonationID());
+            donationDtos.add(dto);
+        }
+        return donationDtos;
     }
 }
